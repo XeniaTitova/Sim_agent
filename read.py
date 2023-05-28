e3bc2d384e1d4265340693842b41b662d2e88faf
XeniaTitova <xenia.titova@epfl.ch>
2023-05-28 07:17:36 +0200

    fichier read est fonctionelle

--- read.py
+++ read.py
@@ -3,18 +3,12 @@ from numpy import load
 from os import listdir
 import os
 
-path =os.getcwd()+"/output_prerender/"#scid_1dde730d8ab3ae4a__aid_1__atype_1.npz"
+path =os.getcwd()+"/out/"#scid_1dde730d8ab3ae4a__aid_1__atype_1.npz"
 list = listdir(path)
-for l in  list :
+for l in list :
     print('\n \n '+l+'\n')
     data = load(path+l)
     lst = data.files
     for item in lst:
         print(item)
         print(data[item])
-        # if item == 'scenario_id':
-        #     print(f'scenario_id :{data[item]}')
-        # elif item == 'agent_id':
-        #     print(f'agent_id :{data[item]}')
-        #print(item)
-        #print(data[item])
